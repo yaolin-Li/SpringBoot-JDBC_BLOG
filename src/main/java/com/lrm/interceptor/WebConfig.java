fb1a3bf06c8111840dd577a7c496f87971833412
Yaolin_Li <1586505611@qq.com>
2020-10-26 21:17:26 +0800

    1
    1

--- src/main/java/com/lrm/interceptor/WebConfig.java
+++ src/main/java/com/lrm/interceptor/WebConfig.java
@@ -2,6 +2,7 @@ package com.lrm.interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
